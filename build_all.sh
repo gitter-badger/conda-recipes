063c75804ad5d0c9810b7987551f5cead0659745
mq <mquade@uni-potsdam.de>
2016-02-03 20:43:34 +0100

    update

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-
+VERSIONS="27 33 34"
+BUILDDIR=~/miniconda3/conda-bld
 for D in *; do
-    if [ -d "${D}" ]; then
-        echo "building ${D}"
-        conda build --py 2.7 --py 3.4 --py 3.5 ${D}
-    fi
+	if [ -d "${D}" ]; then
+	echo "building ${D}"
+	for VERSION in $VERSIONS; do
+		conda build ${D} --python=$VERSION -q
+		TARBALL_PATH=`conda build ${D} --python=$VERSION --output`
+		TARBALL_NAME=`basename $TARBALL_PATH`
+		conda convert $TARBALL_PATH -o ${BUILDDIR} --platform=all -q
+		for platfrom in "win-32" "win-64" "osx-64" "linux-32" "linux-64"; do
+			anaconda upload --force ${BUILDDIR}/$platfrom/${TARBALL_NAME} 
+		done
+	done
+	fi
 done
